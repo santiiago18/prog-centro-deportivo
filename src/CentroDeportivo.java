f1fb575a28498b6e66c838347170d93374b6016e
santiiago18 <scardaya@myuax.com>
2026-03-03 09:11:03 +0100

    feat (CentroDeportivo): Creada la clase CentroDeportivo con constructor

--- src/CentroDeportivo.java
+++ src/CentroDeportivo.java
@@ -1,4 +1,13 @@
-package PACKAGE_NAME;
-
 public class CentroDeportivo {
+
+    private Entrenador[] entrenadores;
+    private final int NUM_MAX_ENTRENADORES;
+
+    public CentroDeportivo(int cantidad_maxima_de_entrenadores) {
+
+        NUM_MAX_ENTRENADORES = cantidad_maxima_de_entrenadores;
+        entrenadores = new Entrenador[NUM_MAX_ENTRENADORES];
+
+    }
+
 }
