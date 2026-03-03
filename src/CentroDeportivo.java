d0e3351a1e9f75e365eea3d9fdb886795120bd07
santiiago18 <scardaya@myuax.com>
2026-03-03 09:16:09 +0100

    feat (CentroDeportivo): Creada el metodo buscarEntrenador(int id) creado

--- src/CentroDeportivo.java
+++ src/CentroDeportivo.java
@@ -7,7 +7,25 @@ public class CentroDeportivo {
 
         NUM_MAX_ENTRENADORES = cantidad_maxima_de_entrenadores;
         entrenadores = new Entrenador[NUM_MAX_ENTRENADORES];
+    }
 
+
+    /**
+     * Busca el primer entrenador que encuentre
+     * @param id identificador del entrenador
+     * @return devueve el id del entrenador si lo encuentra, null si no lo encuentra
+     */
+    public Entrenador buscarEntrenador(int id){
+        boolean seguirBuscando = true;
+        Entrenador entrenador = null;
+
+        for (int i = 0; i < NUM_MAX_ENTRENADORES && seguirBuscando; i++) {
+            if (entrenadores[i].getId() == id) {
+                seguirBuscando = false;
+                entrenador = entrenadores[i];
+            }
+        }
+        return entrenador;
     }
 
 }
