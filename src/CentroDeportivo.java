25892649bc347c5026cb972bf52988ecab23a3fc
santiiago18 <scardaya@myuax.com>
2026-03-03 09:28:42 +0100

    feat (CentroDeportivo): Creada el metodo buscarPrimerHuecoLibre() creado

--- src/CentroDeportivo.java
+++ src/CentroDeportivo.java
@@ -28,4 +28,25 @@ public class CentroDeportivo {
         return entrenador;
     }
 
+    /**
+     * Busca el primer hueco libre
+     * @return el indice del primer hueco libre si existe, y -1 si no existe
+     */
+    private int buscarPrimerHuecoLibre(){
+        int primerHuecoLibre = -1;
+        boolean seguirBuscando = true;
+
+        for (int i = 0; i < NUM_MAX_ENTRENADORES && seguirBuscando; i++) {
+            if (entrenadores[i] == null) {
+                primerHuecoLibre = i;
+                seguirBuscando = false;
+            }
+        }
+        return primerHuecoLibre;
+    }
+
+
+
+
+
 }
