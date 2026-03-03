f3835ecd6ad0d60e247e569b7879ebe30539214e
santiiago18 <scardaya@myuax.com>
2026-03-03 09:07:38 +0100

    doc (Bibliotecta): Comentarios del constructor

--- src/Entrenador.java
+++ src/Entrenador.java
@@ -1,8 +1,19 @@
 public class Entrenador {
+
+    //Atributos de la clase
     private int id;
     private String nombre;
     private String especialidad;
     private int aniosExperiencia;
+
+
+    /**
+     * Metodo constructor
+     * @param id identificador del entrenador
+     * @param nombre nombre del entrenador
+     * @param especialidad especialidad del anetrenador
+     * @param aniosExperiencia años de experiencia del entrenador
+     */
 
     public Entrenador(int id, String nombre, String especialidad,  int aniosExperiencia) {
         this.id = id;
@@ -12,22 +23,43 @@ public class Entrenador {
     }
 
 
+    /**
+     * Devuelve el id del entrenador
+     * @return id de entrenador
+     */
     public int getId() {
         return id;
     }
 
+    /**
+     * Devuelve el nombre del entrenador
+     * @return nombre de entrenador
+     */
     public String getNombre() {
         return nombre;
     }
 
+    /**
+     * Devuelve la especialidad del entrenador
+     * @return especialidad del entrenador
+     */
     public String getEspecialidad() {
         return especialidad;
     }
 
+    /**
+     * Devuelve los años de experiencia del entrenador
+     * @return años de experiencia del entrenador
+     */
     public int getAniosExperiencia() {
         return aniosExperiencia;
     }
 
+
+    /**
+     * Devuelve el texto del entrenador con todos sus atributos
+     * @return cadena de texto con la información del entrenador
+     */
     public String toString(){
         StringBuilder sb = new StringBuilder();
 
